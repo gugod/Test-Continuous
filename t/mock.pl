4f9a65edaf85440752a2456a5c5366efea5bba4d
Kang-min Liu <gugod@gugod.org>
2011-12-17 16:15:09 +0800

    use tempname..

--- t/mock.pl
+++ t/mock.pl
@@ -1,10 +1,10 @@
 use FindBin;
-use File::Temp qw(tempfile);
+use File::Temp qw(:POSIX);
 use YAML;
 
 $ENV{PERL5LIB} = join(":", $FindBin::Bin."/lib", $FindBin::Bin."/../lib", $ENV{PERL5LIB});
 
-$ENV{PERL_TEST_CONTINUOUS_TEST_NOTIFY_OUTPUT} = "/tmp/tcnotification$$";
+$ENV{PERL_TEST_CONTINUOUS_TEST_NOTIFY_OUTPUT} = tmpnam;
 
 {
     open my $fh, ">", $ENV{PERL_TEST_CONTINUOUS_TEST_NOTIFY_OUTPUT};
